586107de3efbc455e497b676c90de990af83ee78
cla <cla@localhost.localdomain>
2015-10-15 16:12:05 +0200

    setup_toolchain uses gcc's contrib script to download dependencies

--- util/setup_toolchain.sh
+++ util/setup_toolchain.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+# TODO disable documentation building (texinfo requirement)
+
 . ../kdevrc
 
 BINUTIL_VERSION="2.25"
 GCC_VERSION="4.9.3"
-GMP_VERSION="6.0.0"
-MPFR_VERSION="3.1.3"
-MPC_VERSION="1.0.3"
 
 TARGET="i686-elf"
 
@@ -17,64 +16,29 @@ mkdir ./src
 
 # Fetch binutils
 
-if [ ! -d "./src/binutils-${BINUTIL_VERSION}" ]; then
+if [ ! -e "./tar/binutils-${BINUTIL_VERSION}.tar.gz" ]; then
     echo "Fetching binutils-${BINUTIL_VERSION}"
     wget -P ./tar "http://ftp.gnu.org/gnu/binutils/binutils-${BINUTIL_VERSION}.tar.gz"
+fi
+    
+if [ ! -d "./src/binutils-${BINUTIL_VERSION}" ]; then
     tar -xf "./tar/binutils-${BINUTIL_VERSION}.tar.gz" -C ./src
 fi
 
 # Fetch gcc
 
-if [ ! -d "./src/gcc-${GCC_VERSION}" ]; then
+if [ ! -e "./tar/gcc-${GCC_VERSION}.tar.bz2" ]; then
     echo "Fetching gcc-${GCC_VERSION}"
     wget -P ./tar "ftp://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.bz2"
+fi
+
+if [ ! -d "./src/gcc-${GCC_VERSION}" ]; then
     tar -xf "./tar/gcc-${GCC_VERSION}.tar.bz2" -C ./src
 fi
 
-# Fetch gmp
-
-if [ ! -d "src/gcc-${GCC_VERSION}/gmp" ]; then
-    
-    if [ ! -d "./src/gmp-${GMP_VERSION}" ]; then
-	echo "Fetching gmp-${GMP_VERSION}"
-	wget -P ./tar "https://gmplib.org/download/gmp/gmp-${GMP_VERSION}a.tar.bz2"
-	tar -xf "./tar/gmp-${GMP_VERSION}a.tar.bz2" -C ./src
-    fi
-    
-    echo "Moving gmp-${GMP_VERSION} to gcc-${GCC_VERSION}/gmp"
-    mv "src/gmp-${GMP_VERSION}" "src/gcc-${GCC_VERSION}/gmp"
-fi
-
-# Fetch mpfr
-
-
-if [ ! -d "src/gcc-${GCC_VERSION}/mpfr" ]; then
-
-    if [ ! -d "./src/mpfr-${MPFR_VERSION}" ]; then
-	echo "Fetching mpfr-${MPFR_VERSION}"
-	wget -P ./tar "http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.bz2"
-	tar -xf "./tar/mpfr-${MPFR_VERSION}.tar.bz2" -C ./src
-    fi
-
-    echo "Moving mpfr-${MPFR_VERSION} to gcc-${GCC_VERSION}/mpfr"
-    mv "src/mpfr-${MPFR_VERSION}" "src/gcc-${GCC_VERSION}/mpfr"
-
-fi
-
-# Fetch mpc
-
-if [ ! -d "src/gcc-${GCC_VERSION}/mpc" ]; then
-
-    if [ ! -d "./src/mpc-${MPC_VERSION}" ]; then
-	echo "Fetching mpc-${MPC_VERSION}"
-	wget -P ./tar "ftp://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz"
-	tar -xf "./tar/mpc-${MPC_VERSION}.tar.gz" -C ./src
-    fi
-
-    echo "Moving mpc-${MPC_VERSION} to gcc-${GCC_VERSION}"
-    mv "src/mpc-${MPC_VERSION}" "src/gcc-${GCC_VERSION}/mpc"
-
-fi
+cd src/gcc-${GCC_VERSION}
+contrib/download_prerequisites
+cd ../..
 
 # Compiling Binutils
 
